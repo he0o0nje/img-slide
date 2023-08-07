6bb19fdba6ca79c98189a56130976a2ecfa93d63
he0o0nje <dudwogjswp@naver.com>
2023-08-07 15:24:28 +0900

    2

--- js/imgslide.js
+++ js/imgslide.js
@@ -4,28 +4,53 @@ window.onload = function () {
     const slideli = slider.querySelectorAll('li');
     const controller = imgslide.querySelector('.controller');
 
-    const liWidth = slideli[0].clientWidth;
-    const sliderwidth = liWidth * slideli.length;
-    slider.style.width = `${sliderwidth}px`;
+    const copy1 = slideli[0].cloneNode(true);
+    const copylast = slideli[slideli.length - 1].cloneNode(true);
+    slider.insertBefore(copylast, slideli[0]);
+    slider.appendChild(copy1);
 
     let currentIdx = 0;
     let translate = 0;
+    const speedTime = 500;
+
+    const slidercopyli = slider.querySelectorAll('li');
+    const liWidth = slideli[0].clientWidth;
+    const sliderwidth = liWidth * slidercopyli.length;
+    slider.style.width = `${sliderwidth}px`;
+    currentIdx = 1;
+    translate = -liWidth;
+    slider.style.transform = `translateX(${translate}px)`;
+
     controller.addEventListener('click', moveSlide);
 
-    function moveSlide(event) {
+    function move(D) {
+        currentIdx += (-1 * D);
+        translate += liWidth * D;
+        slider.style.transform = `translateX(${translate}px)`;
+        slider.style.transition = `all ${speedTime}ms ease`
+      }
+  
+      function moveSlide(event) {
         event.preventDefault();
         if (event.target.className === 'next') {
-            if (currentIdx !== slideli.length - 1) {
-                translate -= liWidth;
+          move(-1);
+          if (currentIdx === slidercopyli.length -1)
+            setTimeout(() => {
+              slider.style.transition = 'none';
+              currentIdx = 1;
+              translate = -liWidth;
+              slider.style.transform = `translateX(${translate}px)`;
+            }, speedTime);
+        } else {
+            move(1);
+            if (currentIdx === 0) {
+              setTimeout(() => {
+                slider.style.transition = 'none';
+                currentIdx = slidercopyli.length -2;
+                translate = -(liWidth * currentIdx);
                 slider.style.transform = `translateX(${translate}px)`;
-                currentIdx += 1;
+              }, speedTime);
             }
-        } else if (event.target.className === 'prev') {
-            if (currentIdx !== 0) {
-                translate += liWidth;
-                slider.style.transform = `translateX(${translate}px)`;
-                currentIdx -= 1;
-            }
-        }
-    }
+          }
+      }
 };
